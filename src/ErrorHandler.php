92bc824379312000f50bf14ab76253e6d2964871
Leo Lu <luyuxuanleo@gmail.com>
2018-09-26 22:34:09 +0800

    fixed bugs

--- src/ErrorHandler.php
+++ src/ErrorHandler.php
@@ -1,8 +1,11 @@
 <?php
 
 /*
- * This file is a part of leoleoasd/laravel-api.
- * Copyright (C) 2019 leoleoasd
+ * This file is part of the leoleoasd/laravel-api.
+ *
+ * (c) Leo Lu <luyuxuanleo@gmail.com>
+ *
+ * This source file is subject to the GPLV3 license that is bundled.
  */
 
 namespace Leoleoasd\LaravelApi;
@@ -14,16 +17,15 @@ class ErrorHandler extends ExceptionHandler
 {
     public function render($request, Exception $exception)
     {
-        if (!$request->isAPI) {
+        if(!$request->isAPI)
             return parent::render($request, $exception);
-        }
-        $r = ResponseJar::make($exception->data ?? [], $exception->errorCode ?? -1, get_class($exception).' '.$exception->getMessage(), $exception->statusCode ?? 500,
+        $r = ResponseJar::make($exception->data ?? [], $exception->errorCode ?? -1, (get_class($exception) != "Excecption" ? get_class($exception) : "").' '.$exception->getMessage(), $exception->statusCode ?? 500,
                 [
                     'request' => $request->all(),
-                    'trace' => json_decode(json_encode($exception->getTrace())) 
+                    'trace' => json_decode(json_encode($exception->getTrace()))
                 ]
             );
-
         return $r->makeResponse();
     }
 }
+
